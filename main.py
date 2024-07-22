baef4cb367bfba5caea6728d0c77a318adca1503
romanafanasyev <rafanya.pvl@gmail.com>
2024-07-22 17:39:20 +0300

    infinite game

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ from wordMidpointFinder import WordMidpointFinder
 model_path = 'navec_hudlit_v1_12B_500K_300d_100q.tar'
 # model_path = 'navec_news_v1_1B_250K_300d_100q.tar'
 finder = WordMidpointFinder(model_path, log=True)
-word1 = 'напиток'
-word2 = 'дерево'
 
-closest_words = finder.find_words_closest_to_midpoint(word1, word2, top_k=10)
-print(f"Words closest to the midpoint of '{word1}' and '{word2}': {closest_words}")
+counter = 1
+while True:
+    word1 = input()
+    word2 = input()
+
+    closest_words = finder.find_words_closest_to_midpoint(word1, word2, top_k=10)
+    print(f"STEP №'{counter}': {closest_words}")
+    counter += 1
